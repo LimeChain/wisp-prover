a484d2b29c59af0c29b146b544e67593f0fe3777
Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>
2023-02-11 16:36:26 +0200

    feat(sh): update circuit name
    Signed-off-by: Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>

--- get-circuits.sh
+++ get-circuits.sh
@@ -2,13 +2,13 @@
 
 # Download the tar file from the Amazon S3 bucket
 echo "Starting download of compiled-circuits.tar from Amazon S3 bucket crc-circuits..."
-curl https://s3.amazonaws.com/crc-circuits/compiled-circuits.tar -o compiled-circuits.tar
+curl https://s3.amazonaws.com/crc-circuits/circuits.tar -o circuits.tar
 
 # Unzip the tar file into the ./circuits directory
 echo "Unzipping compiled-circuits.tar into ./circuits directory..."
 mkdir -p ./circuits
-tar -xvf compiled-circuits.tar -C ./circuits
+tar -xvf circuits.tar -C ./circuits
 
 # Remove the downloaded tar file
 echo "Deleting compiled-circuits.tar..."
-rm compiled-circuits.tar
+rm circuits.tar
